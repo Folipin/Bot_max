4770a914d7da6c1176723712be4d1bf2c1e69bb9
Folipin <123087059+Folipin@users.noreply.github.com>
2026-09-22 17:37:20 +0500

    Update bot_info.js

--- bot_info.js
+++ bot_info.js
@@ -1,1 +1,31 @@
-// JavaScript source code
+const fetch = require("node-fetch");
+
+
+const TOKEN = "вставь токен";
+
+async function getBotInfo() {
+    try {
+        const response = await fetch("https://platform-api2.max.ru/me", {
+            headers: {
+                "Authorization": TOKEN
+            }
+        });
+
+        if (!response.ok) {
+            console.error("Ошибка:", response.status, await response.text());
+            return;
+        }
+
+        const botInfo = await response.json();
+        console.log("✅ Информация о вашем боте:");
+        console.log(`🤖 Имя (отображаемое): ${botInfo.first_name}`);
+        console.log(`🔗 Никнейм (username): @${botInfo.username}`);
+        console.log(`🆔 ID бота (user_id): ${botInfo.user_id}`);
+        console.log(`📝 Описание: ${botInfo.description || "Нет описания"}`);
+
+    } catch (error) {
+        console.error("Ошибка сети:", error.message);
+    }
+}
+
+getBotInfo();// JavaScript source code
